8f1417ab181b1aac9fe5676942ecb9e47bda7ee2
objektwerks <objektwerks@runbox.com>
2016-12-29 08:35:26 -0500

    refactored test conf to mem h2 and dropped future wait times to 1 second

--- src/test/scala/slick/SlickTest.scala
+++ src/test/scala/slick/SlickTest.scala
@@ -10,14 +10,10 @@ class SlickTest extends FunSuite with BeforeAndAfterAll {
   val respository = new Repository(ConfigFactory.load("test.conf"), "test")
   import respository._
 
-  override protected def beforeAll(): Unit = {
-    val future = db.run(createSchema)
-    Await.ready(future, 3 seconds)
-  }
+  override protected def beforeAll(): Unit = Await.ready(db.run(createSchema), 2 seconds)
 
   override protected def afterAll(): Unit = {
-    val future = db.run(dropSchema)
-    Await.ready(future, 3 seconds)
+    Await.ready(db.run(dropSchema), 2 seconds)
     db.close()
   }
 
@@ -25,32 +21,32 @@ class SlickTest extends FunSuite with BeforeAndAfterAll {
     val fred = Person(name = "fred")
     val barney = Person(name = "barney")
 
-    Await.ready(db.run(upsert(fred)), 3 seconds)
-    Await.ready(db.run(upsert(barney)), 3 seconds)
+    Await.ready(db.run(upsert(fred)), 1 second)
+    Await.ready(db.run(upsert(barney)), 1 second)
   }
 
   test("find person > upsert task") {
     val futureFred = db.run(findPerson("fred"))
     val futureBarney = db.run(findPerson("barney"))
 
-    val fred = Await.ready(futureFred, 3 seconds).value.get.get
-    val barney = Await.ready(futureBarney, 3 seconds).value.get.get
+    val fred = Await.ready(futureFred, 1 second).value.get.get
+    val barney = Await.ready(futureBarney, 1 second).value.get.get
 
     val futureFredTask = db.run(upsert(Task(personId = fred.id.get, task = "Mow yard.")))
     val futureBarneyTask = db.run(upsert(Task(personId = barney.id.get, task = "Clean pool.")))
 
-    Await.ready(futureFredTask, 3 seconds)
-    Await.ready(futureBarneyTask, 3 seconds)
+    Await.ready(futureFredTask, 1 second)
+    Await.ready(futureBarneyTask, 1 second)
   }
 
   test("list persons and tasks") {
     val futurePersons = db.run(listPersons)
-    val persons = Await.ready(futurePersons, 3 seconds).value.get.get
+    val persons = Await.ready(futurePersons, 1 second).value.get.get
     assert(persons.size == 2)
     for (p <- persons) {
       println(p)
       val futureTasks = db.run(listTasks(p))
-      val tasks = Await.ready(futureTasks, 3 seconds).value.get.get
+      val tasks = Await.ready(futureTasks, 1 second).value.get.get
       assert(tasks.size == 1)
       for (t <- tasks) {
         println(s"\t$t")
