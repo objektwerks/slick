7f22a2700a73411b0cb0a16d5566df478130c483
objektwerks <objektwerks@runbox.com>
2016-12-29 07:07:38 -0500

    renamed tests

--- src/test/scala/slick/SlickTest.scala
+++ src/test/scala/slick/SlickTest.scala
@@ -19,30 +19,39 @@ class SlickTest extends FunSuite with BeforeAndAfterAll {
     close()
   }
 
-  test("insert person") {
+  test("upsert person") {
     val fred = Person(name = "fred")
     val barney = Person(name = "barney")
+
     val futureFred = db.run(upsert(fred))
     val futureBarney = db.run(upsert(barney))
+
     val fredId = Await.ready(futureFred, Duration.Inf).value.get.get
     val barneyId = Await.ready(futureBarney, Duration.Inf).value.get.get
+
     println(s"Fred inserted autoinc id: $fredId")
     println(s"Barney inserted autoinc id: $barneyId")
+
     assert(fredId > 0)
     assert(barneyId > 0)
   }
 
-  test("insert task") {
+  test("upsert task") {
     val futureFred = db.run(findPerson("fred"))
     val futureBarney = db.run(findPerson("barney"))
+
     val fred = Await.ready(futureFred, Duration.Inf).value.get.get
     val barney = Await.ready(futureBarney, Duration.Inf).value.get.get
+
     val futureFredTask = db.run(upsert(Task(personId = fred.id.get, task = "Mow yard.")))
     val futureBarneyTask = db.run(upsert(Task(personId = barney.id.get, task = "Clean pool.")))
+
     val fredTaskId = Await.ready(futureFredTask, Duration.Inf).value.get.get
     val barneyTaskId = Await.ready(futureBarneyTask, Duration.Inf).value.get.get
+
     println(s"Fred inserted task autoinc id: $fredTaskId")
     println(s"Barney inserted task autoinc id: $barneyTaskId")
+
     assert(fredTaskId > 0)
     assert(barneyTaskId > 0)
   }
@@ -55,6 +64,7 @@ class SlickTest extends FunSuite with BeforeAndAfterAll {
       println(p)
       val futureTasks = db.run(listTasks(p))
       val tasks = Await.ready(futureTasks, Duration.Inf).value.get.get
+
       assert(tasks.size == 1)
       for (t <- tasks) {
         println(s"\t$t")
