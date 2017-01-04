b1c6a6cc259e34e6b9c483ee58c5fc85502e8933
objektwerks <objektwerks@runbox.com>
2017-01-04 08:24:47 -0500

    added schema trait

--- src/test/scala/slick/Repository.scala
+++ src/test/scala/slick/Repository.scala
@@ -8,8 +8,31 @@ import slick.jdbc.H2Profile.api._
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
-class Repository(db: Database) {
-  private implicit val LocalDateTimeMapper = MappedColumnType.base[LocalDateTime, Timestamp](l => Timestamp.valueOf(l), t => t.toLocalDateTime)
+trait Schema {
+  implicit val LocalDateTimeMapper = MappedColumnType.base[LocalDateTime, Timestamp](l => Timestamp.valueOf(l), t => t.toLocalDateTime)
+
+  case class Person(id: Option[Int] = None, name: String)
+
+  class Persons(tag: Tag) extends Table[Person](tag, "persons") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name", O.Unique)
+    def * = (id.?, name) <> (Person.tupled, Person.unapply)
+  }
+
+  case class Task(id: Option[Int] = None, personId: Int, task: String, assigned: LocalDateTime = LocalDateTime.now, completed: Option[LocalDateTime] = None)
+
+  class Tasks(tag: Tag) extends Table[Task](tag, "tasks") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def personId = column[Int]("person_id")
+    def task = column[String]("task")
+    def assigned = column[LocalDateTime]("assigned")
+    def completed = column[Option[LocalDateTime]]("completed")
+    def * = (id.?, personId, task, assigned, completed) <> (Task.tupled, Task.unapply)
+    def person = foreignKey("person_fk", personId, TableQuery[Persons])(_.id)
+  }
+}
+
+class Repository(db: Database) extends Schema {
   private val persons = TableQuery[Persons]
   private val tasks = TableQuery[Tasks]
   private val schema = persons.schema ++ tasks.schema
@@ -40,25 +63,5 @@ class Repository(db: Database) {
       t <- tasks if p.id === t.personId
     } yield (p.name, t.task)
     db.run(query.result)
-  }
-
-  case class Person(id: Option[Int] = None, name: String)
-
-  class Persons(tag: Tag) extends Table[Person](tag, "persons") {
-    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-    def name = column[String]("name", O.Unique)
-    def * = (id.?, name) <> (Person.tupled, Person.unapply)
-  }
-
-  case class Task(id: Option[Int] = None, personId: Int, task: String, assigned: LocalDateTime = LocalDateTime.now, completed: Option[LocalDateTime] = None)
-
-  class Tasks(tag: Tag) extends Table[Task](tag, "tasks") {
-    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-    def personId = column[Int]("person_id")
-    def task = column[String]("task")
-    def assigned = column[LocalDateTime]("assigned")
-    def completed = column[Option[LocalDateTime]]("completed")
-    def * = (id.?, personId, task, assigned, completed) <> (Task.tupled, Task.unapply)
-    def person = foreignKey("person_fk", personId, TableQuery[Persons])(_.id)
   }
 }
