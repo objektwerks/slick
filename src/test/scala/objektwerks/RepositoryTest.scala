4dd7d0d9a63f3c0f8a54697c484cb726a3a42844
objektwerks <objektwerks@runbox.com>
2024-04-30 07:45:40 -0400

    refactored repository test

--- src/test/scala/objektwerks/RepositoryTest.scala
+++ src/test/scala/objektwerks/RepositoryTest.scala
@@ -2,7 +2,6 @@ package objektwerks
 
 import com.typesafe.config.ConfigFactory
 
-import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -12,13 +11,10 @@ import slick.jdbc.{H2Profile, JdbcProfile}
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-final class RepositoryTest extends AnyFunSuite with BeforeAndAfterAll with Matchers:
-  val config = DatabaseConfig.forConfig[JdbcProfile]("test", ConfigFactory.load("test.conf"))
-  val repository = Repository(config, H2Profile, 1 second)
-
-  override protected def beforeAll(): Unit =
+final class RepositoryTest extends AnyFunSuite with Matchers:
+  test("repository"):
+    val config = DatabaseConfig.forConfig[JdbcProfile]("test", ConfigFactory.load("test.conf"))
+    val repository = Repository(config, H2Profile, 1 second)
     repository.createSchema()
-
-  override protected def afterAll(): Unit =
     repository.dropSchema()
     repository.close()
