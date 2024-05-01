cad86343d442af7764da0f51b25c2cc9d0217b07
objektwerks <objektwerks@runbox.com>
2024-05-01 09:38:02 -0400

    edited imports

--- src/test/scala/objektwerks/RepositoryTest.scala
+++ src/test/scala/objektwerks/RepositoryTest.scala
@@ -5,11 +5,11 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import slick.basic.DatabaseConfig
-import slick.jdbc.{H2Profile, JdbcProfile}
-
 import scala.concurrent.duration._
 import scala.language.postfixOps
+
+import slick.basic.DatabaseConfig
+import slick.jdbc.{H2Profile, JdbcProfile}
 
 final class RepositoryTest extends AnyFunSuite with Matchers:
   test("repository"):
